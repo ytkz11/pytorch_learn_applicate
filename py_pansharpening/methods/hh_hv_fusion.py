4886431d95b88d5e4f1a595b1baff40ca3c6bcda
ytkz11 <ytkz11@163.com>
2022-06-06 17:30:21 +0800

    SAR均值滤波程序编写；IHS融合程序编写；

--- py_pansharpening/methods/hh_hv_fusion.py
+++ py_pansharpening/methods/hh_hv_fusion.py
@@ -12,6 +12,7 @@ import os
 import cv2
 from scipy.ndimage.filters import uniform_filter
 
+
 from scipy.ndimage.measurements import variance
 # 百分之二的线性拉伸
 def linear_stretch(data, num=1):
@@ -142,6 +143,7 @@ def main():
     original_1 = mss_ds.GetRasterBand(1).ReadAsArray()
     original_2 = mss_ds.GetRasterBand(2).ReadAsArray()
     original_3 = mss_ds.GetRasterBand(3).ReadAsArray()
+    original_4 = mss_ds.GetRasterBand(4).ReadAsArray()
     sar_1 = ds.GetRasterBand(1).ReadAsArray()
     sar_2 = ds.GetRasterBand(2).ReadAsArray()
     sar_3 = ds.GetRasterBand(3).ReadAsArray()
@@ -159,21 +161,28 @@ def main():
     lee1 = lee_filter(used_1,(int(rows/1), int(cols/1)))
     lee2 = lee_filter(used_2,(int(rows/1), int(cols/1)))
     lee3 = lee_filter(used_3,(int(rows/1), int(cols/1)))
-
+    means_2= means_conv(lee2)
 
 
     lee1 = linear_stretch(lee1, num=2)
-    lee2 = linear_stretch(lee2, num=2)
+    means_2 = linear_stretch(means_2, num=2)
     lee3 = linear_stretch(lee3, num=2)
     plt.imshow(lee2), plt.savefig('lee filter.png', dpi=600), plt.show()
+    plt.imshow(means_2), plt.savefig('lee means filter.png', dpi=600), plt.show()
     temp_arr = np.zeros(shape= (int(rows/1), int(cols/1), 4))
-    temp_arr[:, :, 0] = (lee1 +original_1)/2
-    temp_arr[:, :, 1] = (lee2 +original_2)/2
-    temp_arr[:, :, 2] = (lee3 +original_3)/2
-    temp_arr[:, :, 3] = original_3
-
-
-    filename = os.path.splitext(hh)[0] + '_resize_mss_hhhv.tif'
+    temp_arr[:, :, 0] = (means_2 +original_1)/2
+    temp_arr[:, :, 1] = (means_2 +original_2)/2
+    temp_arr[:, :, 2] = (means_2 +original_3)/2
+    temp_arr[:, :, 3] = original_4
+    temp_arr_ihs = np.zeros(shape=(3,int(rows / 1), int(cols / 1)))
+    temp_arr_ihs[0,:,:] = original_1
+    temp_arr_ihs[1,:,:] = original_2
+    temp_arr_ihs[2,:,:] = original_3
+    RGB = IHS(temp_arr_ihs, means_2)
+    temp_arr[:, :, 0] =RGB[0,:,:]
+    temp_arr[:, :, 1] = RGB[1,:,:]
+    temp_arr[:, :, 2] = RGB[2,:,:]
+    filename = os.path.splitext(hh)[0] + '_resize_mss_hhhv_IHS.tif'
     geoTransform = mss_ds.GetGeoTransform()
     ListgeoTransform = list(geoTransform)
     newgeoTransform = tuple(ListgeoTransform)
@@ -181,7 +190,21 @@ def main():
     proj = mss_ds.GetProjection()
     saveTif(temp_arr, int(cols/1), int(rows/1), driver, proj, newgeoTransform, filename, band=4)
 
+def means_conv(arr):
+    x, y = arr.shape
+    new_arr = np.zeros(shape=(x,y))
+    arr = np.pad(arr, (1, 1), 'edge')  # 填充
+    x, y = arr.shape
 
+    for i in range(1, x - 4):
+        for j in range(1, y - 4):
+            temp = arr[i-1:i+4,j-1:j+4]
+            # a = [m for m in temp.flatten() if m > np.mean(temp)]
+            new_arr[i - 1, j - 1] = np.mean(temp)
+
+
+    plt.imshow(new_arr), plt.title('test'), plt.show()
+    return new_arr
 def combine_gf3():
     hh = 'X:\DengKaiYuan\henan\GF3\GF3_SAY_FSII_025440_E113.7_N35.1_20210609_L2_HHHV_L20005692256\GF3_SAY_FSII_025440_E113.7_N35.1_20210609_L2_HH_L20005692256.tiff'
     hv = 'X:\DengKaiYuan\henan\GF3\GF3_SAY_FSII_025440_E113.7_N35.1_20210609_L2_HHHV_L20005692256\GF3_SAY_FSII_025440_E113.7_N35.1_20210609_L2_HV_L20005692256.tiff'
@@ -240,6 +263,34 @@ def combine_gf3():
 
         raise
 
+
+def IHS(data_low,data_high):
+    """
+    基于IHS变换融合算法
+    输入：np.ndArray格式的三维数组
+    返回：可绘出图像的utf-8格式的三维数组
+    """
+    A = [[1./3.,1./3.,1./3.],[-np.sqrt(2)/6.,-np.sqrt(2)/6.,2*np.sqrt(2)/6],[1./np.sqrt(2),-1./np.sqrt(2),0.]]
+    #RGB－>IHS正变换矩阵
+    B = [[1.,-1./np.sqrt(2),1./np.sqrt(2)],[1.,-1./np.sqrt(2),-1./np.sqrt(2)],[1.,np.sqrt(2),0.]]
+    #IHS－>RGB逆变换矩阵
+    A = np.matrix(A)
+    B = np.matrix(B)
+    w , h = data_high.shape
+    pixels = w * h
+    data_low = data_low.reshape(3,pixels)
+    data_high = data_high.flatten()
+    # data_high = data_high.reshape(pixels)
+    # a1 = np.dot(A , np.matrix(data_high))#高分影像正变换
+    a2 = np.dot(A , np.matrix(data_low))#低分影像正变换
+    a2[0,:] = data_high#用高分影像第一波段替换低分影像第一波段
+    RGB = np.array(np.dot(B , a2))#融合影像逆变换
+    RGB = RGB.reshape((3,w,h))
+    min_val = np.min(RGB.ravel())
+    max_val = np.max(RGB.ravel())
+    RGB = np.uint8((RGB.astype(np.float) - min_val) / (max_val - min_val) * 255)
+    # RGB = Image.fromarray(cv2.merge([RGB[0],RGB[1],RGB[2]]))
+    return RGB
 
 if __name__ == '__main__':
     main()
